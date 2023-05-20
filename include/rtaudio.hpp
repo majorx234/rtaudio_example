303a1a7f6afa50a119f9d528908cecf80e65e7b2
Majorx234 <majorx234@googlemail.com>
2023-05-20 13:06:28 +0200

    Add skeleton to read and write samples from commandline

---
+++ include/rtaudio.hpp
@@ -0,0 +1,8 @@
+#ifndef RTAUDIO_HPP_
+#define RTAUDIO_HPP_
+#include <vector>
+
+std::vector<float> read_data();
+void write_data(std::vector<float> values_data);
+
+#endif // RTAUDIO_HPP_
---
+++ src/rtaudio.cpp
@@ -0,0 +1,10 @@
+#include "rtaudio.hpp"
+
+std::vector<float> read_data(){
+  std::vector<float> values_data{0.0};
+  return values_data;
+}
+
+void write_data(std::vector<float> values_data){
+
+}
